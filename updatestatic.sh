dff200f2dda05545c37a3bad605430f2c5b28f9d
Gonzalo Ruiz <rgon@outlook.com>
2019-08-07 13:17:25 +0200

    Update updatestatic.sh

--- updatestatic.sh
+++ updatestatic.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+git pull
+# Modify the currently-executed file's permissions (and probably its contents)? Let's try. Don't modify lines 1-4.
+chmod +x updatestatic.sh
+
 CONTAINERID=$(docker ps | grep 'momentolive' | awk '{ print $1 }')
 #docker cp ./static ${CONTAINERID}:/var/www/momentolive
 docker cp ./livestreamPlayer.html ${CONTAINERID}:/var/www/momentolive/index.html
