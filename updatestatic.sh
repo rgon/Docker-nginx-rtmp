84b968d7f65e78065b4db09c7c387038628af8ed
Gonzalo Ruiz <rgon@outlook.com>
2019-08-07 12:51:18 +0200

    Update updatestatic.sh

--- updatestatic.sh
+++ updatestatic.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CONTAINERID=$(docker ps | grep 'momentolive' | awk '{ print $1 }')
-docker cp ./static ${CONTAINERID}:/var/www/momentolive
+#docker cp ./static ${CONTAINERID}:/var/www/momentolive
+docker cp ./livestreamPlayer.html ${CONTAINERID}:/var/www/momentolive/index.html
