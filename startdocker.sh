9d0750aa818237cdedc0f5f11558c4782fa066d2
Gonzalo Ruiz <rgon@outlook.com>
2019-08-07 12:57:27 +0200

    kill running containers

--- startdocker.sh
+++ startdocker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 git pull
 docker build -t momentolive .
+
+PREVCONTAINER=$(docker ps | grep 'momentolive' | awk '{ print $1 }')
+docker kill $PREVCONTAINER
+
 sudo docker run -d -p 1935:1935 -p 8080:8080 -e DASH=true momentolive
